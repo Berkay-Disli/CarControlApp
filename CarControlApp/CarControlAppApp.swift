d21c9509c6adfb0b42e0a2b37831676baf522d00
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-02 16:52:37 +0300

    Initial Commit

---
+++ CarControlApp/CarControlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarControlAppApp.swift
+//  CarControlApp
+//
+//  Created by Berkay Disli on 2.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarControlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
